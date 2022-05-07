97d3518f4a978b6e05f3800e3e8485fcb3822cc6
Ryota Higashi <ryo19870130@outlook.jp>
2022-05-07 15:19:08 +0900

    削除

--- src/main/kotlin/com/book/manager/manager/presentation/controller/AdminBookController.kt
+++ src/main/kotlin/com/book/manager/manager/presentation/controller/AdminBookController.kt
@@ -5,6 +5,8 @@ import com.book.manager.infrastructure.database.repository.Book
 import com.book.manager.manager.presentation.form.RegisterBookRequest
 import com.book.manager.manager.presentation.form.UpdateBookRequest
 import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -31,5 +33,9 @@ class AdminBookController (
     @PutMapping("/update")
     fun update(@RequestBody request: UpdateBookRequest) {
         adminBookService.update(request.id, request.title, request.author, request.releaseDate)
+    }
+    @DeleteMapping("/delete/{book_id}")
+    fun delete(@PathVariable("book_id") bookId: Long) {
+        adminBookService.delete(bookId)
     }
 }
